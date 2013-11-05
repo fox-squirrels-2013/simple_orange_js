145042ffa2ffc7865fff18ff52ef1ee7002d8149
Apprentice <apprentice@dbc10.local>
2013-11-05 10:23:01 -0800

    Tree constructor made, with tests on three tree objects

--- orange.js
+++ orange.js
@@ -1,2 +1,84 @@
-// Write your Orange tree code here 
+// Write your Orange tree code here
+
+function Tree(){
+  this.age = 0;
+  this.height = 0;
+  this.alive = true;
+  this.oranges = 0;
+  this.pickOrange = function (){};
+  this.anyOranges = function (){};
+  this.ageAYear = function(){
+    this.age += 1;
+    this.height += Math.random()*3;
+    this.oranges += Math.ceil(Math.random()*4);
+  };
+  this.isDead = function (){
+    if (this.age > 45){
+      this.alive = false;
+    };
+    return this.alive
+  };
+};
+
+var tree = new Tree();
+// var yearOldTree = tree.ageAYear()
+// if (tree.ageAYear) {
+//   tree.age===1
+// };
+var treeTwo = new Tree();
+treeTwo.ageAYear()
+// console.log(treeTwo.oranges)
+
+var treeFifty = new Tree();
+treeFifty.ageAYear();
+treeFifty.ageAYear();
+treeFifty.ageAYear();
+treeFifty.ageAYear();
+treeFifty.ageAYear();
+treeFifty.ageAYear();
+treeFifty.ageAYear();
+treeFifty.ageAYear();
+treeFifty.ageAYear();
+treeFifty.ageAYear();
+treeFifty.ageAYear();
+treeFifty.ageAYear();
+treeFifty.ageAYear();
+treeFifty.ageAYear();
+treeFifty.ageAYear();
+treeFifty.ageAYear();
+treeFifty.ageAYear();
+treeFifty.ageAYear();
+treeFifty.ageAYear();
+treeFifty.ageAYear();
+treeFifty.ageAYear();
+treeFifty.ageAYear();
+treeFifty.ageAYear();
+treeFifty.ageAYear();
+treeFifty.ageAYear();
+treeFifty.ageAYear();
+treeFifty.ageAYear();
+treeFifty.ageAYear();
+treeFifty.ageAYear();
+treeFifty.ageAYear();
+treeFifty.ageAYear();
+treeFifty.ageAYear();
+treeFifty.ageAYear();
+treeFifty.ageAYear();
+treeFifty.ageAYear();
+treeFifty.ageAYear();
+treeFifty.ageAYear();
+treeFifty.ageAYear();
+treeFifty.ageAYear();
+treeFifty.ageAYear();
+treeFifty.ageAYear();
+treeFifty.ageAYear();
+treeFifty.ageAYear();
+treeFifty.ageAYear();
+treeFifty.ageAYear();
+treeFifty.ageAYear();
+treeFifty.ageAYear();
+treeFifty.ageAYear();
+treeFifty.ageAYear();
+treeFifty.ageAYear();
+console.log(treeFifty.age)
 
--- orange_spec.js
+++ orange_spec.js
@@ -5,4 +5,15 @@ function assert(outcome,description){
 // Write your TDD code here  - remember: arrange, act, assert.
 
 
-assert(tree.age==0, "it creates a new tree with age 0")
+assert(tree.age===0, "it creates a new tree with age 0")
+assert(tree.height===0, "it creates a new tree with height 0")
+assert(tree.pickOrange !== undefined, "it creates a new tree with pickOrange method defined")
+assert(tree.anyOranges !== undefined, "it creates a new tree with anyOranges method defined")
+assert(tree.ageAYear !== undefined, "it creates a new tree with ageAYear method defined")
+assert(tree.isDead !== undefined, "it creates a new tree with a isDead method defined")
+assert(tree.alive===true, "it creates a new tree that is alive by default")
+assert(tree.oranges===0, "it creates a new tree that has no oranges by deafult")
+assert(treeTwo.age===1, "a tree that has ageAYear called on it ages by one year")
+assert(treeTwo.height !== 0, "a tree that has had ageAYear called on it increase in height")
+assert(treeTwo.oranges !== 0, "a tree that has had ageAYear called on it has oranges")
+assert(treeFifty.isDead() === true, "a tree that is fifty years old has probably died already")
