5de922ef4aadf9b234b34432716c1264dde8e035
Apprentice <apprentice@dbc-27.local>
2013-11-05 10:27:40 -0800

    Javascript: A journey to the belly of the beast

--- orange.js
+++ orange.js
@@ -1,2 +1,29 @@
-// Write your Orange tree code here 
+tree=new Object();
+    tree.type = "orange";
+    tree.color = "green";
+    tree.age = 0;
+    tree.height = 10;
+    tree.oranges = 0;
+    tree.anyOranges = false;
+    // tree.pickAnOrange = ;
+
+
+function grow()
+{
+var newHeight = tree.height += 2;
+var newAge = tree.age += 1;
+var orangeCount = tree.oranges += 4;
+return newAge, newHeight, orangeCount;
+}
+
+function growOrange() {
+if tree.oranges > 0
+  return tree.anyOranges = true;
+} else {
+  return tree.anyOranges = false;
+}
+
+grow(tree)
+// grow(tree)
+// growOrange(tree)
 
