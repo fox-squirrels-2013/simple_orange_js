5b3d6243ddde31f81709500e610f0b17323e338b
marklevi <mdayanlevi@gmail.com>
2013-11-05 09:41:01 -0800

    initial setup w comments to set a target for orange tree features and add links.md file

---
+++ links.md
@@ -0,0 +1,7 @@
+Google Chrome has a ton of features if you click the config gears icon on 
+the bottom right hand side of the Developer Console (CMD-OPT-J).
+
+There's a link in there to shortcuts that work in the console and i think
+some of these will be useful.  
+
+[shortcuts in google chrome developer console](https://developers.google.com/chrome-developer-tools/docs/shortcuts)
--- orange.js
+++ orange.js
@@ -1,2 +1,29 @@
 // Write your Orange tree code here 
 
+// tree = OrangeTree.new
+
+// tree.age! until tree.any_oranges?
+
+// puts "Tree is #{tree.age} years old and #{tree.height} feet tall"
+
+// until tree.dead?
+//   basket = []
+
+//   # It places the oranges in the basket
+//   # IT PLACES THE ORANGES IN THE BASKET
+//   while tree.any_oranges?
+//     basket << tree.pick_an_orange!
+//   end
+
+//   avg_diameter = # It's up to you to calculate the average diameter for this harvest.
+
+//   puts "Year #{tree.age} Report"
+//   puts "Tree height: #{tree.height} feet"
+//   puts "Harvest:     #{basket.size} oranges with an average diameter of #{avg_diameter} inches"
+//   puts ""
+
+//   # Age the tree another year
+//   tree.age!
+// end
+
+// puts "Alas, the tree, she is dead!"
